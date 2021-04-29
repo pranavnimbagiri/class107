57a217932e5ebf4f7d9adfdb8039f08273ae6c02
pranavnimbagiri <67331631+pranavnimbagiri@users.noreply.github.com>
2021-04-29 17:36:31 +0530

    Add files via upload

---
+++ datavisualization.py
@@ -0,0 +1,16 @@
+import csv
+import pandas
+import plotly.graph_objects as go
+df=pandas.read_csv("data.csv")
+print(df.groupby("level")["attempt"].mean())
+fig=go.Figure(go.Bar(
+    x=df.groupby("level")["attempt"].mean(),
+    y=['level 1','level 2','level 3','level 4'],orientation='h'
+))
+#fig.show()
+student_df=df.loc[df['student_id']=="TRL_987"]
+fig=go.Figure(go.Bar(
+    x=student_df.groupby("level")["attempt"].mean(),
+    y=['level 1','level 2','level 3','level 4'],orientation='h'
+))
+fig.show()
